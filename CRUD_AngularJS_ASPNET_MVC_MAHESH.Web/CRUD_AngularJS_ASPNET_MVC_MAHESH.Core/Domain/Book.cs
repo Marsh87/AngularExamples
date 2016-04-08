407c137db9bcc2b822426f21b7a82c88d0a23fea
unknown <Mahesh@Mahesh.chilli.local>
2016-04-08 14:11:46 +0200

    Added DIY Angular app

---
+++ CRUD_AngularJS_ASPNET_MVC_MAHESH.Web/CRUD_AngularJS_ASPNET_MVC_MAHESH.Core/Domain/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_AngularJS_ASPNET_MVC_MAHESH.Core.Domain
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Ibsn { get; set; }
+    }
+}
---
+++ CRUD_AngularJS_ASPNET_MVC_MAHESH.Web/CRUD_AngularJS_ASPNET_MVC_MAHESH.Web/Controllers/BookController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Web.Mvc;
+using CRUD_AngularJS_ASPNET_MVC_MAHESH.Core.Repositories.Interfaces;
+
+namespace CRUD_AngularJS_ASPNET_MVC_MAHESH.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookController(IBookRepository bookRepository)
+        {
+           if (bookRepository == null) throw new ArgumentNullException(nameof(bookRepository));
+            _bookRepository = bookRepository;
+        }
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
